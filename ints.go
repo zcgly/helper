8cf32f482f2e82ef7918c8e37ea09f61e5d2fc1e
zcgly <1910256@qq.com>
2024-01-16 12:38:31 +0800

    feat:StrToInts

--- ints.go
+++ ints.go
@@ -1,5 +1,32 @@
 package helper
 
+import (
+	"strconv"
+	"strings"
+)
+
+func StrToInts(s string, sep string) []int {
+	if s == "" {
+		return []int{}
+	}
+
+	items := strings.Split(s, sep)
+	itemLen := len(items)
+	ret := make([]int, 0, itemLen)
+	for i := range items {
+		item := strings.TrimSpace(items[i])
+		if item == "" {
+			continue
+		}
+		val, err := strconv.Atoi(item)
+		if err != nil {
+			panic(err)
+		}
+		ret = append(ret, val)
+	}
+	return ret
+}
+
 func UpperTo2Power(num int) int {
 	//return 1 << uint(math.Ceil(math.Log2(float64(num))))
 	num--
@@ -12,11 +39,18 @@ func UpperTo2Power(num int) int {
 }
 
 func Is2Power(num int) bool {
+	if num <= 0 {
+		return false
+	}
 	return num&(num-1) == 0
 	//return num & -num == num
 }
 
 // 超过半数
+
 func MoreThanHalf(num int) int {
+	if num <= 0 {
+		return 0
+	}
 	return num/2 + 1
 }
--- ints_test.go
+++ ints_test.go
@@ -48,3 +48,78 @@ func TestUpperTo2Power1(t *testing.T) {
 		})
 	}
 }
+
+func TestStrToInts(t *testing.T) {
+	type args struct {
+		s   string
+		sep string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"empty", args{s: "", sep: ","}, []int{}},
+		{"1", args{s: "1", sep: ","}, []int{1}},
+		{"2", args{s: "2,3", sep: ","}, []int{2, 3}},
+		{"有空格", args{s: "2, 3", sep: ","}, []int{2, 3}},
+		{"多余的逗号", args{s: ",2,3,", sep: ","}, []int{2, 3}},
+		//{"无效", args{s: "2a,3", sep: ","}, []int{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, StrToInts(tt.args.s, tt.args.sep), "StrToInts(%v, %v)", tt.args.s, tt.args.sep)
+		})
+	}
+}
+
+func TestIs2Power(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"0", args{num: 0}, false},
+		{"1", args{num: 1}, true},
+		{"2", args{num: 2}, true},
+		{"3", args{num: 3}, false},
+		{"4", args{num: 4}, true},
+		{"5", args{num: 5}, false},
+		{"6", args{num: 6}, false},
+		{"7", args{num: 7}, false},
+		{"8", args{num: 8}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, Is2Power(tt.args.num), "Is2Power(%v)", tt.args.num)
+		})
+	}
+}
+
+func TestMoreThanHalf(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"0", args{num: 0}, 0},
+		{"1", args{num: 1}, 1},
+		{"2", args{num: 2}, 2},
+		{"3", args{num: 3}, 2},
+		{"4", args{num: 4}, 3},
+		{"5", args{num: 5}, 3},
+		{"6", args{num: 6}, 4},
+		{"7", args{num: 7}, 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, MoreThanHalf(tt.args.num), "MoreThanHalf(%v)", tt.args.num)
+		})
+	}
+}
